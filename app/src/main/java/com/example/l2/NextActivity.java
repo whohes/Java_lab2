4fdd63c772dcf182b809905810fbaf6fc7d6a955
whohes <dencelf_2007@mail.ru>
2024-06-05 15:21:01 +0600

    Initial commit

---
+++ app/src/main/java/com/example/l2/NextActivity.java
@@ -0,0 +1,55 @@
+package com.example.l2;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Toast;
+public class NextActivity extends AppCompatActivity {
+    private Button btn2;
+    private ImageView imageview;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_next);
+
+        imageview = findViewById(R.id.imageView2);
+        imageview.setImageDrawable(null);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        addListenerOnButton();
+    }
+    public void addListenerOnButton(){
+        btn2 = findViewById(R.id.button3);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageview.setImageResource(R.drawable.orangecat);
+            }
+        });
+    }
+
+
+    public void goBack(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+
+        startActivity(intent);
+    }
+
+}
